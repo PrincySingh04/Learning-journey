e43a0056aeb959a3e9f35f42396ceb43a46ab6ea
Princy Singh <princys549@gmail.com>
2026-04-24 20:42:55 +0530

    Added JavaScript practice files from VS Code
    This repository documents my daily learning journey in programming and data-related technologies. 
    I am currently building my skills in :
    JavaScript
    Here, I regularly upload practice problems, small programs, and mini projects to improve my problem-solving ability and coding consistency

---
+++ dayy1.js
@@ -0,0 +1,34 @@
+// declare variable
+x=10;
+console.log(x)
+// creating 2 variables
+a=5
+b=3
+c=a+b
+console.log(c)
+
+// let 
+let y=10;
+console.log(x);
+
+let name='princy';
+console.log('my name is :' + name)
+
+let z=10;
+z=11;
+console.log(z)
+
+// swaping of two numbers
+let p=10;
+let q=20;
+ [p,q]=[q,p];
+console.log(p,q)
+// bill calculation
+
+let price =100;
+let quantity=3;
+let gst=0.18;
+let total=price*quantity
+let finalAmount=total+(total*gst);
+console.log(finalAmount)
+
